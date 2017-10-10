4d49c8b1e91d5264b1c7784b6c0c4e2fa2d8e109
Michael Lipscombe <mlipscombe@outlook.com>
2017-10-10 12:00:58 -0700

    Added NetResource

--- SMBFileShareClient/Program.cs
+++ SMBFileShareClient/Program.cs
@@ -10,6 +10,35 @@ namespace SMBFileShareClient
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to network resource...");
+
+                    var ret = NetResource.CopyFile("SMBFileShareClient.pdb", @"\\10.0.0.31\mike", @"PHATGOCODE\FileRepositoryUser", "^Slinky4$");
+                    if (ret != null)
+                    {
+                        Console.Error.WriteLine("Error returned: " + ret);
+                    }
+
+                    Console.WriteLine("Success!");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("Error: " + ex);
+                }
+                finally
+                {
+                    Console.WriteLine("*** END ***");
+                    Console.Out.Flush();
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+           
+
+
         }
     }
 }
